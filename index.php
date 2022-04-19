a11ec8d1c972a0743c7a8e1b66310438b8889697
Raphael Kalinowski <raphael@myminicom.com>
2022-04-19 16:14:54 +0200

    added class NewArray

--- index.php
+++ index.php
@@ -3,15 +3,19 @@
 require "vendor/autoload.php";
 
 use Raphael\Src\Models\Dev;
+use Raphael\Src\Models\NewArray;
 use Michelf\Markdown;
 
-echo Markdown::defaultTransform("Ici le test pour installation d'une **librairie** depuis php simple");
+echo Markdown::defaultTransform("Ici le test pour installation d'une **librairie markdown** et ci-dessous des collections :");
+
+$array = NewArray::createArray();
 
 $raph = new Dev('Raphael', 'Kalinowski');
 echo $raph->getFullName();
-echo '<br>';
 $raph->setRole('junior');
-echo '<br>';
 echo $raph->getRole();
 
 dump($raph);
+dump($array);
+
+/*collect($array)*/
