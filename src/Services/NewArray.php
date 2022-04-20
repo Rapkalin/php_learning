96ef64b3fa807685149c5e2261e0fda694c4f5cb
Raphael Kalinowski <raphael@myminicom.com>
2022-04-20 10:12:04 +0200

    Refacto function for collect

--- src/Services/NewArray.php
+++ src/Services/NewArray.php
@@ -54,31 +54,26 @@ class NewArray
 //        return $array;
 //    }
 
+    /**
+     * @param \Illuminate\Support\Collection $collection
+     * @param int $data
+     * @return \Illuminate\Support\Collection
+     */
     public static function addMiddleCollect(\Illuminate\Support\Collection $collection, int $data) : \Illuminate\Support\Collection
     {
         // étape 1 : Je récupère la key correspondant à la moitié du tableau que j'arrondis
         $middleKey = (int)round((($collection->count())-1)/2);
-
-        // étape 2 : j'enregistre la partie retirée dans une variable
         $chunks = $collection->chunk($middleKey);
+        $newCollect = $chunks->all()[0];
+        $newCollect
+            ->push($data)
+            ->push();
+                $chunks
+                    ->all()[1]
+                    ->each( function ($value) use ($newCollect) {
+                        $newCollect->push($value);
+                    });
 
-        $newcollect = $chunks->all()[0];
-        dump($newcollect); // a vérifier pour récup la 1ère partie de chunk (ie. chhubk[0]) et push la second partie apres avoir ajouté la data
-        $newcollect->push($data)->push($chunks->all()[1]);
-
-        dump($chunks->all());
-        dump($chunks->all()[1]);
-
-        // étape 3 : J'enlève la partie du tableau qui se situe après la 2ème moitié ($middleKey) du tableau
-//        $array = array_slice($array, 0, $middleKey);
-
-        // étape 4 : J'ajoute le nouvel élément dans le tableau
-//        $array[] = $data;
-
-        // étape 5 : J'ajoute la partie retirée en étape 1 et j'enregistre
-//        foreach($removedPart as $value) {
-//            $array[] = $value;
-//        }
-        return $collection;
+        return $newCollect;
     }
 }
